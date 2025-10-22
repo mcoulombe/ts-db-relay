ee013d087c2f8ad0b702ef392dd571dccd1fde3e
mcoulombe <max@tailscale.com>
2025-10-22 06:21:37 +0100

    * switch to openbao dependency

--- relays-postgres.go
+++ relays-postgres.go
@@ -22,8 +22,8 @@ import (
 	"strings"
 	"time"
 
-	"github.com/hashicorp/vault/plugins/database/postgresql"
-	"github.com/hashicorp/vault/sdk/database/dbplugin/v5"
+	"github.com/openbao/openbao/plugins/database/postgresql"
+	"github.com/openbao/openbao/sdk/v2/database/dbplugin/v5"
 	"tailscale.com/client/local"
 	"tailscale.com/metrics"
 	"tailscale.com/tailcfg"
